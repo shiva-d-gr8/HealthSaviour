5fca8eccd7c1f96d2cd1fb29cd087718c2b66858
Shiva <shiva9211@gmail.com>
2026-05-29 20:20:16 +0530

    Fix Render frontend build issue

--- server/server.js
+++ server/server.js
@@ -24,12 +24,12 @@ app.use("/api/user", userRouter);
 app.use("/api/doctor", doctorRouter);
 app.use("/api/appointment", appointRouter);
 app.use("/api/notification", notificationRouter);
-app.use(express.static(path.join(__dirname, "./client/build")));
+// app.use(express.static(path.join(__dirname, "./client/build")));
 
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
+// app.get("*", (req, res) => {
+//  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+// });
 
 const server = app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
